e0c4075b872ba0ac2835a53ece4afaac3943be58
Diyorbek <diyorbekabdumannonov4@gmail.com>
2023-06-01 17:45:02 +0500

    updated

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
 jQuery(function ($) {
-    $('button.openPaymentDropdown').click(function (e) {
-        const dropdown = $(e.target).parents('.all_buttons').find(`#${e.target.name}`)
-        console.log(dropdown[0])
-        $('.dropdown').addClass('d-none')
-        dropdown[0].className = 'd-inline-block dropdown'
+    $('button.next').click(function () {
+        var getIndex = $(this).parents('.acc-control').index() + 1;
+        if (getIndex == 1) {
+            const inputEl = $(this).parents('.acc-control').find('input[name="FirtsName"').val()
+            console.log(inputEl)
+            if (inputEl == '') {
+                alert('Please complete all fields');
+            }
+            else {
+                const dropdown = $(`.${getIndex + 1}`)
+                $('.dropdown').addClass('d-none')
+                dropdown[0].className = `d-inline-block dropdown ${getIndex + 1}`
+            }
+        }
+        if (getIndex == 2) {
+            const inputEl = $(this).parents('.acc-control').find('input[name="address"').val()
+            console.log(inputEl)
+            if (inputEl == '') {
+                alert('Please complete all fields');
+            }
+            else {
+                const dropdown = $(`.${getIndex + 1}`)
+                $('.dropdown').addClass('d-none')
+                dropdown[0].className = `d-inline-block dropdown ${getIndex + 1}`
+            }
+        }
+        if (getIndex == 3) {
+
+        }
     })
 })
