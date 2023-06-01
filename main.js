ad13d356b4f771c6096b2040e9ba76ec3330c9b7
Diyorbek <diyorbekabdumannonov4@gmail.com>
2023-06-01 18:36:55 +0500

    changes

--- main.js
+++ main.js
@@ -3,11 +3,26 @@ jQuery(function ($) {
         var getIndex = $(this).parents('.acc-control').index() + 1;
         if (getIndex == 1) {
             const inputEl = $(this).parents('.acc-control').find('input[name="FirtsName"').val()
-            console.log(inputEl)
+
+            var ownerEmail = $(this).parents('.inner_tab_content').find('input[type="email"]').val();
+            var validatePass = false;
+            var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
+            if (testEmail.test(ownerEmail)) {
+                validatePass = true;
+            } else {
+                validatePass = false;
+            }
             if (inputEl == '') {
                 alert('Please complete all fields');
             }
+            else if (ownerEmail == '') {
+                alert('please enter the email');
+            } else if (ownerEmail != '' && validatePass == false) {
+                alert('Not a valid email address');
+            }
             else {
+                $(`.fst`).find('.check').removeClass('d-none')
+                $(`.snd`).find('.plus').addClass('d-none')
                 const dropdown = $(`.${getIndex + 1}`)
                 $('.dropdown').addClass('d-none')
                 dropdown[0].className = `d-inline-block dropdown ${getIndex + 1}`
@@ -15,18 +30,25 @@ jQuery(function ($) {
         }
         if (getIndex == 2) {
             const inputEl = $(this).parents('.acc-control').find('input[name="address"').val()
-            console.log(inputEl)
             if (inputEl == '') {
                 alert('Please complete all fields');
             }
             else {
+                $(`.snd`).find('.check').removeClass('d-none')
+                $(`.thr`).find('.plus').addClass('d-none')
                 const dropdown = $(`.${getIndex + 1}`)
                 $('.dropdown').addClass('d-none')
                 dropdown[0].className = `d-inline-block dropdown ${getIndex + 1}`
             }
         }
         if (getIndex == 3) {
-
+            const inputEl = $(this).parents('.acc-control').find('input[name="payInfo"').val()
+            if (inputEl == '') {
+                alert('Please complete all fields');
+            }
+            else {
+                $(`.thr`).find('.check').removeClass('d-none')
+            }
         }
     })
 })
