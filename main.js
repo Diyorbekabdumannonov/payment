d0c4bc723baa7b1051e78d68b0bcdde1e6adb181
Diyorbek <diyorbekabdumannonov4@gmail.com>
2023-06-06 16:29:03 +0500

    chanegs

--- main.js
+++ main.js
@@ -125,6 +125,90 @@ jQuery(function ($) {
       }
     }
   });
+
+  $(".openPaymentDropdown").click(function () {
+    var nextIndex = $(this).parents(".acc-control").index() + 1;
+    console.log($(".acc-control .dropdown"));
+    if (nextIndex == 3) {
+      if ($(".acc-control .dropdown")[0].classList.contains("d-inline-block")) {
+        const inputEl = $(".acc-control").find('input[name="FirtsName"').val();
+
+        var ownerEmail = $(this)
+          .parents(".inner_tab_content")
+          .find('input[type="email"]')
+          .val();
+        var validatePass = false;
+        var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
+        if (testEmail.test(ownerEmail)) {
+          validatePass = true;
+        } else {
+          validatePass = false;
+        }
+        if (inputEl == "") {
+          alert("Please complete all fields");
+        } else if (ownerEmail == "") {
+          alert("please enter the email");
+        } else if (ownerEmail != "" && validatePass == false) {
+          alert("Not a valid email address");
+        } else {
+          $(`.fst`).find(".check").removeClass("d-none");
+          $(`.snd`).find(".plus").addClass("d-none");
+          const dropdown = $(`.2`);
+          $(".dropdown").addClass("d-none");
+          dropdown[0].className = `d-inline-block dropdown 2`;
+        }
+      }
+      if ($(".acc-control .dropdown")[1].classList.contains("d-inline-block")) {
+        const inputEl = $(".acc-control").find('input[name="address"').val();
+        if (inputEl == "") {
+          alert("Please complete all fields");
+        } else {
+          $(`.snd`).find(".check").removeClass("d-none");
+          $(`.thr`).find(".plus").addClass("d-none");
+          const dropdown = $(`.3`);
+          $(".dropdown").addClass("d-none");
+          dropdown[0].className = `d-inline-block dropdown 3`;
+        }
+      }
+    } else if (nextIndex == 2) {
+      const inputEl = $(this)
+        .parents(".acc-control")
+        .find('input[name="FirtsName"')
+        .val();
+
+      var ownerEmail = $(this)
+        .parents(".inner_tab_content")
+        .find('input[type="email"]')
+        .val();
+      var validatePass = false;
+      var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
+      if (testEmail.test(ownerEmail)) {
+        validatePass = true;
+      } else {
+        validatePass = false;
+      }
+      if (inputEl == "") {
+        alert("Please complete all fields");
+      } else if (ownerEmail == "") {
+        alert("please enter the email");
+      } else if (ownerEmail != "" && validatePass == false) {
+        alert("Not a valid email address");
+      } else {
+        $(`.fst`).find(".check").removeClass("d-none");
+        $(`.snd`).find(".plus").addClass("d-none");
+        const dropdown = $(`.${2}`);
+        $(".dropdown").addClass("d-none");
+        dropdown[0].className = `d-inline-block dropdown ${2}`;
+      }
+    } else {
+      $(`.snd`).find(".check").removeClass("d-none");
+      $(`.thr`).find(".plus").addClass("d-none");
+      const dropdown = $(`.${nextIndex}`);
+      $(".dropdown").addClass("d-none");
+      dropdown[0].className = `d-inline-block dropdown ${nextIndex}`;
+    }
+  });
+
   $("a.innner_previous_btn").click(function (e) {
     $(".dropdown").addClass("d-none");
     $(".dropdown.1").addClass("d-inline-block");
